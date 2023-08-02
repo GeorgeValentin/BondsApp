836a15b1885ac721f523ca559bf44362fd87e462
unknown <nora9wang@163.com>
2023-08-02 23:19:42 +0100

    added books data

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -6,3 +6,12 @@ VALUES ('BNPParibasIssu 4,37% Microsoft Corp (USD)'),
 ('HM Treasury United Kingdon'),
 ('TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S'),
 ('First Norway Alpha Kl.IV');
+
+
+INSERT INTO BOOKS (book_name, user_id)
+VALUES ('trading_book_1', 1),
+('trading_book_2', 1),
+('trading_book_3', 1),
+('trading_book_4', 1),
+('trading_book_5', 1),
+('trading_book_6', 1);
