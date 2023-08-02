f1e894d58aaa24576ee258407e3f18c8d2112c6a
Abi <abijay97@gmail.com>
2023-08-02 15:58:56 +0100

    added issuers data

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO ISSUERS(issuer_name)
+VALUES ('BNPParibasIssu 4,37% Microsoft Corp (USD)'),
+('Airbus 3.15%  USD'),
+('UBS Facebook (USD)'),
+('Amazon'),
+('HM Treasury United Kingdon'),
+('TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S'),
+('First Norway Alpha Kl.IV');
