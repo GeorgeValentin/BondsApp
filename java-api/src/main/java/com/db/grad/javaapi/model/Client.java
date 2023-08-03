343d554cc90fb9673a6b9f7c5c71cea9fa96ba30
Oana Butaru <oana-andreea.butaru@campus.tu-berlin.de>
2023-08-03 10:32:09 +0200

    added model for clients

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/Client.java
@@ -0,0 +1,53 @@
+package com.db.grad.javaapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column(name = "bond_holder", nullable = false, length = 250)
+    private String bondHolder;
+
+    @ManyToOne
+    @JoinColumn(name = "issuer_id", referencedColumnName = "issuer_id")
+    private Issuer issuer;
+
+    public Client() {
+
+    }
+    public Client(Long clientId, String bondHolder, Issuer issuer) {
+        this.clientId = clientId;
+        this.bondHolder = bondHolder;
+        this.issuer = issuer;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getBondHolder() {
+        return bondHolder;
+    }
+
+    public void setBondHolder(String bondHolder) {
+        this.bondHolder = bondHolder;
+    }
+
+    public Issuer getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(Issuer issuer) {
+        this.issuer = issuer;
+    }
+}
