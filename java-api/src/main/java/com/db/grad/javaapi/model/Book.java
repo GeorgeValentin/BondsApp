5a48080f4ed5b893b472cc687219dae8a92272db
unknown <nora9wang@163.com>
2023-08-03 09:56:18 +0100

    added book class

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/Book.java
@@ -0,0 +1,44 @@
+package com.db.grad.javaapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private int bookId;
+    @Column(name = "book_name")
+    private String bookName;
+
+    public Book(int bookId, String bookName) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId" + bookId +
+                ", bookName='" + bookName + '\'' +
+                '}';
+    }
+
+}
