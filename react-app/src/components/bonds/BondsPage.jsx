190092ffef185b521853e24d85ef240a885514db
Cujbă George Valentin <george.cujba15@gmail.com>
2023-08-04 12:32:16 +0300

    Configure the service

--- react-app/src/components/bonds/BondsPage.jsx
+++ react-app/src/components/bonds/BondsPage.jsx
@@ -1,13 +1,20 @@
 import { Fragment, useEffect, useState } from 'react';
+import { findActiveBonds } from '../../services/BondServices';
 
 const BondsPage = () => {
   const [bonds, setBonds] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setBonds(findActiveBonds());
+    console.log(bonds);
+  }, []);
 
   return (
     <Fragment>
       <h2>This is the Active Bonds Page where I will display all the bonds</h2>
+      {/* {bonds.map((bond) => (
+        <div>{bond.issuerName}</div>
+      ))} */}
     </Fragment>
   );
 };
