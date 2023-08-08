aa37e7efd9c50161e910820b85aadf8cb426c9f2
Cujbă George Valentin <george.cujba15@gmail.com>
2023-08-08 12:56:17 +0300

    Add the parsing for the settlement date

--- react-app/src/components/bonds/bond-page/BondPage.jsx
+++ react-app/src/components/bonds/bond-page/BondPage.jsx
@@ -34,69 +34,66 @@ const BondPage = () => {
 
   return (
     <Fragment>
-        <div className='bond-page-container'>
+      <div className='bond-page-container'>
+        <div className='bond-data-header'>Bond data</div>
 
-      <div className='bond-data-header'>Bond data</div>
+        <MDBTable className='bond-page-table' hover>
+          <MDBTableHead>
+            <tr>
+              <th scope='col'>Property</th>
+              <th scope='col'>Value</th>
+            </tr>
+          </MDBTableHead>
+          <MDBTableBody>
+            <tr>
+              <td>Bond Id</td>
+              <td>{bondId}</td>
+            </tr>
+            <tr>
+              <td>Client</td>
+              <td>{activeBond.client.bondHolder}</td>
+            </tr>
+            <tr>
+              <td>Issuer</td>
+              <td>{activeBond.client.issuer.issuerName}</td>
+            </tr>
+            <tr>
+              <td>Maturity date</td>
 
-      <MDBTable className='bond-page-table' hover>
-        <MDBTableHead>
-          <tr>
-            <th scope='col'>Property</th>
-            <th scope='col'>Value</th>
-          </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        <tr>
-          <td>Bond Id</td>
-          <td>{bondId}</td>
-        </tr>
-        <tr>
-          <td>Client</td>
-          <td>{activeBond.client.bondHolder}</td>
-        </tr>
-        <tr>
-          <td>Issuer</td>
-          <td>{activeBond.client.issuer.issuerName}</td>
-        </tr>
-        <tr>
-          <td>Maturity date</td>
+              <td>{parseDate(activeBond.bondMaturityDate)}</td>
+            </tr>
+            <tr>
+              <td>Coupon percent</td>
+              <td>{activeBond.couponPercent}</td>
+            </tr>
+            <tr>
+              <td>CUSIP</td>
+              <td>{activeBond.cusip}</td>
+            </tr>
+            <tr>
+              <td>ISIN</td>
+              <td>{activeBond.isin}</td>
+            </tr>
+            <tr>
+              <td>Face value</td>
+              <td>{activeBond.faceValue}</td>
+            </tr>
+            <tr>
+              <td>Trade settlement date</td>
+              <td>{parseDate(activeBond.tradeSettlementDate)}</td>
+            </tr>
+            <tr>
+              <td>Redemption status</td>
+              <td>{activeBond.redemptionStatus}</td>
+            </tr>
+          </MDBTableBody>
+        </MDBTable>
 
-          <td>{parseDate(activeBond.bondMaturityDate)}</td>
-        </tr>
-        <tr>
-          <td>Coupon percent</td>
-          <td>{activeBond.couponPercent}</td>
-        </tr>
-        <tr>
-          <td>CUSIP</td>
-          <td>{activeBond.cusip}</td>
-        </tr>
-        <tr>
-          <td>ISIN</td>
-          <td>{activeBond.isin}</td>
-        </tr>
-          <tr>
-          <td>Face value</td>
-          <td>{activeBond.faceValue}</td>
-        </tr>
-          <tr>
-          <td>Trade settlement date</td>
-          <td>{activeBond.tradeSettlementDate}</td>
-        </tr>
-          <tr>
-          <td>Redemption status</td>
-          <td>{activeBond.redemptionStatus}</td>
-        </tr>
-
-      </MDBTableBody>
-    </MDBTable>
-
-    <Button className='bond-page-trigger-button'>
-      Trigger for redemption
-    </Button>
-    </div>
-</Fragment>
-
+        <Button className='bond-page-trigger-button'>
+          Trigger for redemption
+        </Button>
+      </div>
+    </Fragment>
   );
 };
 
